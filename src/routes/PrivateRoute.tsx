f0dae6b38e4d77852ce3d8ddf0d056ab55004bc0
Adersh M (VALOREM LLC) <v-adershm@microsoft.com>
2025-07-15 22:38:27 +0530

    feat: remove unused files and implement routing with Redux
    - Deleted unused SVG files (vite.svg, react.svg) and App.css.
    - Removed App component and replaced it with AppRoutes for routing.
    - Integrated Redux Provider in main.tsx to manage application state.
    - Added console logs in PrivateRoute for debugging authentication status.
    - Set up Cypress configuration and added initial tests for redirection and fixtures.

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -4,6 +4,8 @@ import { useAppSelector } from "../app/hooks";
 export default function PrivateRoute() {
     const status = useAppSelector((state) => state.auth.status);
     const isAuthenticated = status === "authenticated";
+    console.log("PrivateRoute isAuthenticated:", isAuthenticated);
+    console.log("PrivateRoute status:", status);
     const location = useLocation();
 
     if(!isAuthenticated) {
