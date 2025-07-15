04ee52e56f2e7e1872dd2d99be5dc927d209758c
Adersh M (VALOREM LLC) <v-adershm@microsoft.com>
2025-07-15 22:53:36 +0530

    feat: add wait-on dependency and implement e2e testing workflow

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -4,8 +4,6 @@ import { useAppSelector } from "../app/hooks";
 export default function PrivateRoute() {
     const status = useAppSelector((state) => state.auth.status);
     const isAuthenticated = status === "authenticated";
-    console.log("PrivateRoute isAuthenticated:", isAuthenticated);
-    console.log("PrivateRoute status:", status);
     const location = useLocation();
 
     if(!isAuthenticated) {
